fortanix/rust-sgx

commit 8a051cf19d56972d7d3b133805c8e6fd3f19a259

Split SGX ISA into separate crate

sgxs/Cargo.toml
@@ -22,6 +22,7 @@ with-rust-crypto = ["rust-crypto"]
 "openssl-sys" = { git = "https://github.com/sfackler/rust-openssl", rev = "7610804c" }  # MIT
 "regex" = "0.1"       # MIT/Apache-2.0
 "time" = "0.1"        # MIT/Apache-2.0
+"sgx-isa" = "0.1"     # MIT/Apache-2.0
 
 [dependencies.rust-crypto]  # MIT/Apache-2.0
 version = "0.2"

sgx-isa/Cargo.toml
@@ -1,0 +1,10 @@
+[package]
+name = "sgx-isa"
+description = "Constants and structures related to the Intel SGX ISA extension"
+version = "0.1.0"
+authors = ["Jethro Beekman <jethro@jbeekman.nl>"]
+license = "MIT/Apache-2.0"
+repository = "https://github.com/jethrogb/sgx-utils/tree/master/sgx-isa"
+
+[dependencies]
+"bitflags" = "0.5" # MIT/Apache-2.0

sgxs/src/bin/sgxs-info.rs
@@ -10,13 +10,15 @@
  */
 
 extern crate sgxs as sgxs_crate;
+extern crate sgx_isa;
+
 use std::path::Path;
 use std::fs::File;
 use std::ffi::OsStr;
 use std::fmt;
 
 use sgxs_crate::sgxs::{self,SgxsRead};
-use sgxs_crate::abi::secinfo_flags;
+use sgx_isa::secinfo_flags;
 
 /// Ok(Some(_)) all data is _
 /// Ok(None) there is data, but not all bytes are the same

sgx-isa/.gitignore
@@ -1,0 +1,2 @@
+target
+Cargo.lock

sgx-isa/COPYING-APACHE
@@ -1,0 +1,204 @@
+sgx-isa - Constants and structures related to the Intel SGX ISA extension.
+Copyright (C) 2016  Jethro G. Beekman
+
+                              Apache License
+                        Version 2.0, January 2004
+                     http://www.apache.org/licenses/
+
+TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
+
+1. Definitions.
+
+   "License" shall mean the terms and conditions for use, reproduction,
+   and distribution as defined by Sections 1 through 9 of this document.
+
+   "Licensor" shall mean the copyright owner or entity authorized by
+   the copyright owner that is granting the License.
+
+   "Legal Entity" shall mean the union of the acting entity and all
+   other entities that control, are controlled by, or are under common
+   control with that entity. For the purposes of this definition,
+   "control" means (i) the power, direct or indirect, to cause the
+   direction or management of such entity, whether by contract or
+   otherwise, or (ii) ownership of fifty percent (50%) or more of the
+   outstanding shares, or (iii) beneficial ownership of such entity.
+
+   "You" (or "Your") shall mean an individual or Legal Entity
+   exercising permissions granted by this License.
+
+   "Source" form shall mean the preferred form for making modifications,
+   including but not limited to software source code, documentation
+   source, and configuration files.
+
+   "Object" form shall mean any form resulting from mechanical
+   transformation or translation of a Source form, including but
+   not limited to compiled object code, generated documentation,
+   and conversions to other media types.
+
+   "Work" shall mean the work of authorship, whether in Source or
+   Object form, made available under the License, as indicated by a
+   copyright notice that is included in or attached to the work
+   (an example is provided in the Appendix below).
+
+   "Derivative Works" shall mean any work, whether in Source or Object
+   form, that is based on (or derived from) the Work and for which the
+   editorial revisions, annotations, elaborations, or other modifications
+   represent, as a whole, an original work of authorship. For the purposes
+   of this License, Derivative Works shall not include works that remain
+   separable from, or merely link (or bind by name) to the interfaces of,
+   the Work and Derivative Works thereof.
+
+   "Contribution" shall mean any work of authorship, including
+   the original version of the Work and any modifications or additions
+   to that Work or Derivative Works thereof, that is intentionally
+   submitted to Licensor for inclusion in the Work by the copyright owner
+   or by an individual or Legal Entity authorized to submit on behalf of
+   the copyright owner. For the purposes of this definition, "submitted"
+   means any form of electronic, verbal, or written communication sent
+   to the Licensor or its representatives, including but not limited to
+   communication on electronic mailing lists, source code control systems,
+   and issue tracking systems that are managed by, or on behalf of, the
+   Licensor for the purpose of discussing and improving the Work, but
+   excluding communication that is conspicuously marked or otherwise
+   designated in writing by the copyright owner as "Not a Contribution."
+
+   "Contributor" shall mean Licensor and any individual or Legal Entity
+   on behalf of whom a Contribution has been received by Licensor and
+   subsequently incorporated within the Work.
+
+2. Grant of Copyright License. Subject to the terms and conditions of
+   this License, each Contributor hereby grants to You a perpetual,
+   worldwide, non-exclusive, no-charge, royalty-free, irrevocable
+   copyright license to reproduce, prepare Derivative Works of,
+   publicly display, publicly perform, sublicense, and distribute the
+   Work and such Derivative Works in Source or Object form.
+
+3. Grant of Patent License. Subject to the terms and conditions of
+   this License, each Contributor hereby grants to You a perpetual,
+   worldwide, non-exclusive, no-charge, royalty-free, irrevocable
+   (except as stated in this section) patent license to make, have made,
+   use, offer to sell, sell, import, and otherwise transfer the Work,
+   where such license applies only to those patent claims licensable
+   by such Contributor that are necessarily infringed by their
+   Contribution(s) alone or by combination of their Contribution(s)
+   with the Work to which such Contribution(s) was submitted. If You
+   institute patent litigation against any entity (including a
+   cross-claim or counterclaim in a lawsuit) alleging that the Work
+   or a Contribution incorporated within the Work constitutes direct
+   or contributory patent infringement, then any patent licenses
+   granted to You under this License for that Work shall terminate
+   as of the date such litigation is filed.
+
+4. Redistribution. You may reproduce and distribute copies of the
+   Work or Derivative Works thereof in any medium, with or without
+   modifications, and in Source or Object form, provided that You
+   meet the following conditions:
+
+   (a) You must give any other recipients of the Work or
+       Derivative Works a copy of this License; and
+
+   (b) You must cause any modified files to carry prominent notices
+       stating that You changed the files; and
+
+   (c) You must retain, in the Source form of any Derivative Works
+       that You distribute, all copyright, patent, trademark, and
+       attribution notices from the Source form of the Work,
+       excluding those notices that do not pertain to any part of
+       the Derivative Works; and
+
+   (d) If the Work includes a "NOTICE" text file as part of its
+       distribution, then any Derivative Works that You distribute must
+       include a readable copy of the attribution notices contained
+       within such NOTICE file, excluding those notices that do not
+       pertain to any part of the Derivative Works, in at least one
+       of the following places: within a NOTICE text file distributed
+       as part of the Derivative Works; within the Source form or
+       documentation, if provided along with the Derivative Works; or,
+       within a display generated by the Derivative Works, if and
+       wherever such third-party notices normally appear. The contents
+       of the NOTICE file are for informational purposes only and
+       do not modify the License. You may add Your own attribution
+       notices within Derivative Works that You distribute, alongside
+       or as an addendum to the NOTICE text from the Work, provided
+       that such additional attribution notices cannot be construed
+       as modifying the License.
+
+   You may add Your own copyright statement to Your modifications and
+   may provide additional or different license terms and conditions
+   for use, reproduction, or distribution of Your modifications, or
+   for any such Derivative Works as a whole, provided Your use,
+   reproduction, and distribution of the Work otherwise complies with
+   the conditions stated in this License.
+
+5. Submission of Contributions. Unless You explicitly state otherwise,
+   any Contribution intentionally submitted for inclusion in the Work
+   by You to the Licensor shall be under the terms and conditions of
+   this License, without any additional terms or conditions.
+   Notwithstanding the above, nothing herein shall supersede or modify
+   the terms of any separate license agreement you may have executed
+   with Licensor regarding such Contributions.
+
+6. Trademarks. This License does not grant permission to use the trade
+   names, trademarks, service marks, or product names of the Licensor,
+   except as required for reasonable and customary use in describing the
+   origin of the Work and reproducing the content of the NOTICE file.
+
+7. Disclaimer of Warranty. Unless required by applicable law or
+   agreed to in writing, Licensor provides the Work (and each
+   Contributor provides its Contributions) on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+   implied, including, without limitation, any warranties or conditions
+   of TITLE, NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A
+   PARTICULAR PURPOSE. You are solely responsible for determining the
+   appropriateness of using or redistributing the Work and assume any
+   risks associated with Your exercise of permissions under this License.
+
+8. Limitation of Liability. In no event and under no legal theory,
+   whether in tort (including negligence), contract, or otherwise,
+   unless required by applicable law (such as deliberate and grossly
+   negligent acts) or agreed to in writing, shall any Contributor be
+   liable to You for damages, including any direct, indirect, special,
+   incidental, or consequential damages of any character arising as a
+   result of this License or out of the use or inability to use the
+   Work (including but not limited to damages for loss of goodwill,
+   work stoppage, computer failure or malfunction, or any and all
+   other commercial damages or losses), even if such Contributor
+   has been advised of the possibility of such damages.
+
+9. Accepting Warranty or Additional Liability. While redistributing
+   the Work or Derivative Works thereof, You may choose to offer,
+   and charge a fee for, acceptance of support, warranty, indemnity,
+   or other liability obligations and/or rights consistent with this
+   License. However, in accepting such obligations, You may act only
+   on Your own behalf and on Your sole responsibility, not on behalf
+   of any other Contributor, and only if You agree to indemnify,
+   defend, and hold each Contributor harmless for any liability
+   incurred by, or claims asserted against, such Contributor by reason
+   of your accepting any such warranty or additional liability.
+
+END OF TERMS AND CONDITIONS
+
+APPENDIX: How to apply the Apache License to your work.
+
+   To apply the Apache License to your work, attach the following
+   boilerplate notice, with the fields enclosed by brackets "[]"
+   replaced with your own identifying information. (Don't include
+   the brackets!)  The text should be enclosed in the appropriate
+   comment syntax for the file format. We also recommend that a
+   file or class name and description of purpose be included on the
+   same "printed page" as the copyright notice for easier
+   identification within third-party archives.
+
+Copyright [yyyy] [name of copyright owner]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.

sgx-isa/COPYING-MIT
@@ -1,0 +1,26 @@
+sgx-isa - Constants and structures related to the Intel SGX ISA extension.
+Copyright (C) 2016  Jethro G. Beekman
+
+Permission is hereby granted, free of charge, to any
+person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the
+Software without restriction, including without
+limitation the rights to use, copy, modify, merge,
+publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software
+is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice
+shall be included in all copies or substantial portions
+of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.

sgx-isa/src/lib.rs
@@ -1,15 +1,19 @@
 /*
- * The Intel SGX ABI.
+ * Constants and structures related to the Intel SGX ISA extension.
  *
  * (C) Copyright 2016 Jethro G. Beekman
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; version 2
- * of the License.
+ * Licensed under the Apache License, Version 2.0
+ * <COPYING-APACHE or http://www.apache.org/licenses/LICENSE-2.0> or the MIT
+ * license <COPYING-MIT or http://opensource.org/licenses/MIT>, at your
+ * option. All files in the project carrying such notice may not be copied,
+ * modified, or distributed except according to those terms.
  */
 
-#![allow(dead_code)]
+#![no_std]
+
+#[macro_use]
+extern crate bitflags;
 
 use core::mem::transmute;
 
@@ -49,7 +53,7 @@ pub enum Enclu {
 
 #[derive(Clone,Copy,Debug)]
 #[repr(u32)]
-pub enum ErrorCodes {
+pub enum ErrorCode {
 	Success                =   0,
 	InvalidSigStruct       =   1,
 	InvalidAttribute       =   2,
@@ -97,8 +101,8 @@ pub enum PageType {
 
 #[derive(Clone,Copy,Debug)]
 #[repr(u16)]
-pub enum KeyName {
-	Launch        = 0,
+pub enum Keyname {
+	EinitToken    = 0, // EinitToken in §38.17.1, Launch in §41.3
 	Provision     = 1,
 	ProvisionSeal = 2,
 	Report        = 3,
@@ -136,29 +140,35 @@ pub struct Attributes {
 	pub xfrm: u64,
 }
 
-bitflags! {
-	flags AttributesFlags: u64 {
-		const INIT          = 0b0000_0001,
-		const DEBUG         = 0b0000_0010,
-		const MODE64BIT     = 0b0000_0100,
-		const PROVISIONKEY  = 0b0001_0000,
-		const EINITTOKENKEY = 0b0010_0000,
+pub mod attributes_flags {
+	bitflags! {
+		pub flags AttributesFlags: u64 {
+			const INIT          = 0b0000_0001,
+			const DEBUG         = 0b0000_0010,
+			const MODE64BIT     = 0b0000_0100,
+			const PROVISIONKEY  = 0b0001_0000,
+			const EINITTOKENKEY = 0b0010_0000,
+		}
+	}
+
+	impl Default for AttributesFlags {
+		fn default() -> Self { Self::empty() }
 	}
 }
+pub use self::attributes_flags::AttributesFlags;
 
-impl Default for AttributesFlags {
-	fn default() -> Self { Self::empty() }
-}
+pub mod miscselect {
+	bitflags! {
+		pub flags Miscselect: u32 {
+			const EXINFO = 0b0000_0001,
+		}
+	}
 
-bitflags! {
-	flags Miscselect: u32 {
-		const EXINFO = 0b0000_0001,
+	impl Default for Miscselect {
+		fn default() -> Self { Self::empty() }
 	}
 }
-
-impl Default for Miscselect {
-	fn default() -> Self { Self::empty() }
-}
+pub use self::miscselect::Miscselect;
 
 #[repr(C,packed)]
 // Doesn't work because large array: #[derive(Clone,Debug,Default)]
@@ -183,15 +193,18 @@ impl Default for Tcs {
 	}
 }
 
-bitflags! {
-	flags TcsFlags: u64 {
-		const DBGOPTIN = 0b0000_0001,
+pub mod tcs_flags {
+	bitflags! {
+		pub flags TcsFlags: u64 {
+			const DBGOPTIN = 0b0000_0001,
+		}
+	}
+
+	impl Default for TcsFlags {
+		fn default() -> Self { Self::empty() }
 	}
 }
-
-impl Default for TcsFlags {
-	fn default() -> Self { Self::empty() }
-}
+pub use self::tcs_flags::TcsFlags;
 
 #[repr(C,packed)]
 #[derive(Clone,Debug,Default)]
@@ -216,11 +229,8 @@ impl Default for Secinfo {
 }
 
 pub mod secinfo_flags {
-	use super::PageType;
-	use core::mem::transmute;
-
 	bitflags! {
-		flags SecinfoFlags: u64 {
+		pub flags SecinfoFlags: u64 {
 			const R        = 0b0000_0000_0000_0001,
 			const W        = 0b0000_0000_0000_0010,
 			const X        = 0b0000_0000_0000_0100,
@@ -229,9 +239,9 @@ pub mod secinfo_flags {
 			const PR       = 0b0000_0000_0010_0000,
 			const PT_MASK  = 0b1111_1111_0000_0000,
 			const PT_B0    = 0b0000_0001_0000_0000, // ****
-			const PT_B1    = 0b0000_0010_0000_0000, // * These are just here so that
-			const PT_B2    = 0b0000_0100_0000_0000, // * something shows up in the
-			const PT_B3    = 0b0000_1000_0000_0000, // * Debug output
+			const PT_B1    = 0b0000_0010_0000_0000, // * These are just here so
+			const PT_B2    = 0b0000_0100_0000_0000, // * that something shows
+			const PT_B3    = 0b0000_1000_0000_0000, // * up in the Debug output
 			const PT_B4    = 0b0001_0000_0000_0000, // *
 			const PT_B5    = 0b0010_0000_0000_0000, // *
 			const PT_B6    = 0b0100_0000_0000_0000, // *
@@ -239,12 +249,17 @@ pub mod secinfo_flags {
 		}
 	}
 
+	impl Default for SecinfoFlags {
+		fn default() -> Self { Self::empty() }
+	}
+
 	impl SecinfoFlags {
-		pub fn page_type(&self) -> PageType {
-			unsafe{transmute((((*self&PT_MASK).bits) >> 8) as u8)}
+		pub fn page_type(&self) -> u8 {
+			(((*self&PT_MASK).bits) >> 8) as u8
 		}
 
-		pub fn page_type_mut(&mut self) -> &mut PageType {
+		pub fn page_type_mut(&mut self) -> &mut u8 {
+			use core::mem::transmute;
 			unsafe {
 				let page_type: &mut [u8;8]=transmute(&mut self.bits);
 				transmute(&mut page_type[1])
@@ -252,17 +267,12 @@ pub mod secinfo_flags {
 		}
 	}
 
-	impl From<PageType> for SecinfoFlags {
-		fn from(data: PageType) -> SecinfoFlags {
+	impl From<super::PageType> for SecinfoFlags {
+		fn from(data: super::PageType) -> SecinfoFlags {
 			SecinfoFlags::from_bits_truncate((data as u64)<<8)
 		}
 	}
-
-	impl Default for SecinfoFlags {
-		fn default() -> Self { Self::empty() }
-	}
 }
-
 pub use self::secinfo_flags::SecinfoFlags;
 
 #[repr(C,packed)]
@@ -271,7 +281,7 @@ pub struct Pcmd {
 	pub secinfo:    Secinfo,
 	pub enclaveid:  u64,
 	pub _reserved1: [u8; 40],
-	pub mac:        [u8; 2],
+	pub mac:        [u8; 16],
 }
 
 #[repr(C,packed)]
@@ -303,7 +313,7 @@ pub struct Sigstruct {
 #[repr(C,packed)]
 // Doesn't work because large array: #[derive(Clone,Debug,Default)]
 pub struct Einittoken {
-	pub valid:              u32,
+	pub valid:              u32, // debug in §38.14, valid in §41.3
 	pub _reserved1:         [u8; 44],
 	pub attributes:         Attributes,
 	pub mrenclave:          [u8; 32],
@@ -381,13 +391,16 @@ impl Default for Keyrequest {
 	}
 }
 
-bitflags! {
-	flags Keypolicy: u16 {
-		const MRENCLAVE = 0b0000_0001,
-		const MRSIGNER  = 0b0000_0010,
+pub mod keypolicy {
+	bitflags! {
+		pub flags Keypolicy: u16 {
+			const MRENCLAVE = 0b0000_0001,
+			const MRSIGNER  = 0b0000_0010,
+		}
+	}
+
+	impl Default for Keypolicy {
+		fn default() -> Self { Self::empty() }
 	}
 }
-
-impl Default for Keypolicy {
-	fn default() -> Self { Self::empty() }
-}
+pub use self::keypolicy::Keypolicy;

sgxs/src/bin/pe2sgxs.rs
@@ -16,6 +16,7 @@ extern crate lazy_static;
 extern crate sgxs as sgxs_crate;
 extern crate pe;
 extern crate broadcast;
+extern crate sgx_isa;
 
 use std::fs::File;
 use std::io::{self,Read,Write};
@@ -26,7 +27,7 @@ use std::sync::atomic;
 use broadcast::BroadcastWriter;
 
 use sgxs_crate::crypto::{Sha256Digest,Sha256};
-use sgxs_crate::abi::{Tcs,Sigstruct,PageType,secinfo_flags,SecinfoFlags};
+use sgx_isa::{Tcs,Sigstruct,PageType,secinfo_flags,SecinfoFlags};
 use sgxs_crate::sgxs::{SgxsWrite,CanonicalSgxsWriter,self,SecinfoTruncated};
 
 use pe::types::{section_characteristics,SectionHeader,DataDirectory,DirectoryEntry};

commit 3f5ff1a82370bd681a8e71d1c632d8ddce05622d

Fix PageCharacteristic display (broken in 8a051cf1) and bump sgxs version

sgxs-tools/Cargo.toml
@@ -1,7 +1,7 @@
 [package]
 name = "sgxs-tools"
 description = "Utilities for working with the SGX stream format"
-version = "0.4.0"
+version = "0.4.1"
 authors = ["Jethro Beekman <jethro@jbeekman.nl>"]
 license = "GPL-2.0+"
 repository = "https://github.com/jethrogb/sgx-utils/tree/master/sgxs-tools"
@@ -17,5 +17,5 @@ with-rust-crypto = ["sgxs/rust-crypto"]
 "pe" = "0.1"          # GPL-2.0+
 "clap" = "2.0"        # MIT
 "regex" = "0.1"       # MIT/Apache-2.0
-"sgx-isa" = "0.1"     # MIT/Apache-2.0
+"sgx-isa" = "0.1.2"   # MIT/Apache-2.0
 "num" = "0.1.31"      # MIT/Apache-2.0

sgxs-tools/src/bin/sgxs-info.rs
@@ -18,7 +18,7 @@ use std::ffi::OsStr;
 use std::fmt;
 
 use sgxs_crate::sgxs::{self,SgxsRead};
-use sgx_isa::secinfo_flags;
+use sgx_isa::{PageType,secinfo_flags};
 
 /// Ok(Some(_)) all data is _
 /// Ok(None) there is data, but not all bytes are the same
@@ -114,7 +114,7 @@ impl fmt::Display for PageCharacteristic {
 				if flags.contains(secinfo_flags::X) { perm[2]=b'x'; }
 
 				write!(f,"{:<4} {} {:>7} meas={}",
-					format!("{:?}",flags.page_type()),
+					format!("{:?}",PageType::from_repr(flags.page_type()).unwrap()),
 					unsafe{std::str::from_utf8_unchecked(&perm)},
 					data,
 					measured_chunks

sgxs/Cargo.toml
@@ -1,7 +1,7 @@
 [package]
 name = "sgxs"
 description = "Library for working with the SGX stream format"
-version = "0.4.0"
+version = "0.4.1"
 authors = ["Jethro Beekman <jethro@jbeekman.nl>"]
 license = "GPL-2.0+"
 repository = "https://github.com/jethrogb/sgx-utils/tree/master/sgxs"