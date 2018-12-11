623ff75da6c4ef8fa5ed8c8b67830aa418e42972
Jethro Beekman <jethro@fortanix.com>
2018-12-11 16:44:07 +0530

    Refactor sgxs-loaders to add a builder and factor out common code

--- sgxs-loaders/src/lib.rs
+++ sgxs-loaders/src/lib.rs
@@ -15,4 +15,40 @@ extern crate sgxs as sgxs_crate;
 #[macro_use]
 extern crate failure_derive;
 
+mod generic;
 pub mod isgx;
+
+use std::fmt::Debug;
+use std::os::raw::c_void;
+use std::sync::Arc;
+
+use sgxs_crate::loader;
+
+#[derive(Debug)]
+pub struct Tcs {
+    _mapping: Arc<Debug + Sync + Send>,
+    address: u64,
+}
+
+impl loader::Tcs for Tcs {
+    fn address(&self) -> *mut c_void {
+        self.address as _
+    }
+}
+
+#[derive(Debug)]
+pub struct MappingInfo {
+    _mapping: Arc<Debug + Sync + Send>,
+    base: u64,
+    size: u64,
+}
+
+impl loader::MappingInfo for MappingInfo {
+    fn address(&self) -> *mut c_void {
+        self.base as _
+    }
+
+    fn size(&self) -> usize {
+        self.size as _
+    }
+}
