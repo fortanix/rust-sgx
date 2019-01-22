110333821ab44a2169635fa1facee90e411adfd3
akashfortanix <akash.anand@fortanix.com>
2019-01-22 10:05:19 +0530

    fix manifest parsing & ftxsgx-runner-cargo should never use coresident signature

--- fortanix-sgx-tools/src/bin/ftxsgx-runner.rs
+++ fortanix-sgx-tools/src/bin/ftxsgx-runner.rs
@@ -7,38 +7,57 @@
 extern crate aesm_client;
 extern crate enclave_runner;
 extern crate sgxs_loaders;
-#[macro_use]
 extern crate failure;
-
-use std::path::Path;
+#[macro_use]
+extern crate clap;
 
 use aesm_client::AesmClient;
-use enclave_runner::Command;
+use enclave_runner::EnclaveBuilder;
 use failure::{Error, ResultExt};
 use sgxs_loaders::isgx::Device as IsgxDevice;
 
+use clap::{App, Arg};
+
+arg_enum!{
+    #[derive(PartialEq, Debug)]
+    #[allow(non_camel_case_types)]
+    pub enum Signature {
+        coresident,
+        dummy
+    }
+}
+
 fn main() -> Result<(), Error> {
-    let mut args = std::env::args_os();
-    let cmdname = args.next();
-    let file = match args.next() {
-        Some(arg) => arg,
-        None => {
-            let cmdname = cmdname
-                .as_ref()
-                .and_then(|p| Path::new(p).file_name())
-                .map(|p| Path::new(p).display().to_string());
-            eprintln!(
-                "Usage: {} <ENCLAVE>\n",
-                cmdname.unwrap_or("ftxsgx-runner".into())
-            ); // extra newline
-            bail!("Missing <ENCLAVE> parameter on command line");
-        }
-    };
+    let args = App::new("ftxsgx-runner")
+        .arg(
+            Arg::with_name("file")
+                .required(true)
+        )
+        .arg(Arg::with_name("signature")
+            .short("s")
+            .long("signature")
+            .required(false)
+            .takes_value(true)
+            .possible_values(&Signature::variants()))
+        .get_matches();
+
+    let file = args.value_of("file").unwrap();
+
     let mut device = IsgxDevice::new()
         .context("While opening SGX device")?
         .einittoken_provider(AesmClient::new())
         .build();
-    let enclave = Command::new(&file, &mut device).context("While loading SGX enclave")?;
+
+    let mut enclave_builder = EnclaveBuilder::new(file.as_ref());
+
+    match args.value_of("signature").map(|v| v.parse().expect("validated")) {
+        Some(Signature::coresident) => { enclave_builder.coresident_signature().context("While loading coresident signature")?; }
+        Some(Signature::dummy) => { enclave_builder.dummy_signature(); },
+        None => (),
+    }
+
+    let enclave = enclave_builder.build(&mut device).context("While loading SGX enclave")?;
+
     enclave.run().map_err(|e| {
         println!("Error while executing SGX enclave.\n{}", e);
         std::process::exit(-1)
