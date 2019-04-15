79882d807f4cf9b7cd17620e0ac66fe26e6d233e
Parth Sane <parthsane@gmail.com>
2019-04-15 17:17:49 +0530

    Changed architecture to now have an IO handler thread and worker threads.
        Encapsulated RunningTcs and EnclaveState in another struct called IOHandlerInput
        Added work sender into IOHandlerInput
    
        Moved RunningTcs::entry and RunningTcs::coentry functionality to do_work function inside impl Work
    
        Moved all Coresult analysis and response to the usercall thread
    
        Added tcs_address function to Usercall; refactored code accordingly
    
        Refactored code: available TCSes are now kept in EnclaveState for both libraries and commands
    
        TODO:
        The IO calls are still synchronous, they need to made async
    
        find best way to select num of worker threads
    
        (hack) Modified wait to return immediatly with Ok(event_mask) without error immediately

--- enclave-runner/src/command.rs
+++ enclave-runner/src/command.rs
@@ -20,7 +20,7 @@ pub struct Command {
     threads: Vec<ErasedTcs>,
     address: usize,
     size: usize,
-    usercall_ext : Option<Box<dyn UsercallExtension>>,
+    usercall_ext: Option<Box<dyn UsercallExtension>>,
 }
 
 impl MappingInfo for Command {
@@ -36,8 +36,12 @@ impl MappingInfo for Command {
 impl Command {
     /// # Panics
     /// Panics if the number of TCSs is 0.
-    pub(crate) fn internal_new(mut tcss: Vec<ErasedTcs>, address: *mut c_void, size: usize,
-                      usercall_ext : Option<Box<dyn UsercallExtension>>) -> Command {
+    pub(crate) fn internal_new(
+        mut tcss: Vec<ErasedTcs>,
+        address: *mut c_void,
+        size: usize,
+        usercall_ext: Option<Box<dyn UsercallExtension>>,
+    ) -> Command {
         let main = tcss.remove(0);
         Command {
             main,
