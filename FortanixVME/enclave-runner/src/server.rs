f101343dd046f54f1542bd6cb3669761821a98ef
Raoul Strackx <raoul.strackx@fortanix.com>
2021-09-09 16:03:29 +0200

    Proxying outbound connections

--- FortanixVME/enclave-runner/src/server.rs
+++ FortanixVME/enclave-runner/src/server.rs
@@ -1,9 +1,12 @@
+use nix::sys::select::{select, FdSet};
 use std::thread;
-use std::io::{Error as IoError, Read, Write};
+use std::io::{Error as IoError, ErrorKind as IoErrorKind, Read, Write};
 use std::net::{Shutdown, TcpListener, TcpStream};
-use fortanix_vme_abi::{self, Response, Request};
+use std::os::unix::io::AsRawFd;
+use fortanix_vme_abi::{self, Error, Response, Request};
 
-const READ_BUFF_SIZE: usize = 10;
+const BUFF_SIZE: usize = 1024;
+const PROXY_BUFF_SIZE: usize = 4192;
 
 pub struct Server {
     port: u16,
@@ -17,35 +20,130 @@ impl Server {
     }
 
     fn read_from_stream(stream: &mut TcpStream) -> Result<Vec<u8>, IoError> {
-        let mut buff = Vec::new();
-        buff.resize(READ_BUFF_SIZE, 0);
+        let mut buff = [0; BUFF_SIZE];
         let n = stream.read(&mut buff)?;
-        buff.resize(n, 0);
-        println!("read: {:?} ({}bytes)", buff, n);
-        if n == READ_BUFF_SIZE {
+        let mut buff = buff[0..n].to_vec();
+        //TODO This will block when the n*BUFF_SIZE bytes need to be read
+        if n == BUFF_SIZE {
             buff.append(&mut Self::read_from_stream(stream)?);
         }
         Ok(buff)
     }
 
-    fn read_request(stream: &mut TcpStream) -> Result<Request, IoError> {
+    fn log_communication(src: &str, src_port: u16, dst: &str, dst_port: u16, msg: &str, arrow: &str) {
+        let src = format!("{}:{}", src, src_port);
+        let dst = format!("{}:{}", dst, dst_port);
+        let msg: String = msg.chars().into_iter().take(80).collect();
+        println!("{:>20} {} {:<20}: {:?}", src, arrow, dst, msg);
+    }
+
+    fn read_request(stream: &mut TcpStream) -> Result<Request, Error> {
         let buff = Self::read_from_stream(stream)?;
-        Ok(Request::deserialize(&buff).unwrap())
+        let req = serde_cbor::from_slice(&buff).map_err(|e| Error::DeserializationError(e))?;
+        Self::log_communication(
+            "runner",
+            stream.local_addr().map(|addr| addr.port()).unwrap_or_default(),
+            "enclave",
+            stream.peer_addr().map(|addr| addr.port()).unwrap_or_default(),
+            &format!("{:?}", &req),
+            "<-");
+        Ok(req)
+    }
+
+    fn transfer_data(src: &mut TcpStream, src_name: &str, dst: &mut TcpStream, dst_name: &str) -> Result<usize, IoError> {
+        let mut buff = [0; PROXY_BUFF_SIZE];
+        let n = src.read(&mut buff[..])?;
+        Self::log_communication(
+            "runner",
+            src.local_addr().map(|addr| addr.port()).unwrap_or_default(),
+            src_name,
+            src.peer_addr().map(|addr| addr.port()).unwrap_or_default(),
+            &String::from_utf8(buff[0..n].to_vec()).unwrap_or_default(),
+            "<-");
+        if n > 0 {
+            dst.write_all(&buff[0..n])?;
+            Self::log_communication(
+                dst_name,
+                dst.peer_addr().map(|addr| addr.port()).unwrap_or_default(),
+                "runner",
+                dst.local_addr().map(|addr| addr.port()).unwrap_or_default(),
+                &String::from_utf8(buff[0..n].to_vec()).unwrap_or_default(),
+                "<-");
+        }
+        Ok(n)
+    }
+
+    /*
+     * +-----------+
+     * |   remote  |
+     * +-----------+
+     *       ^
+     *       |
+     *       |
+     *       v
+     * +----[2]-----+            +-------------+
+     * |   Runner   |            |   enclave   |
+     * +--[3]--[1]--+            +-[ ]----[ ]--+
+     *     \    \---- enclave ------/      /
+     *      \-------- proxy --------------/
+     *
+     *  [1] enclave
+     *  [2] remote
+     *  [3] proxy
+     */
+    fn handle_request_connect(remote_addr: &String, enclave: &mut TcpStream) -> Result<(), IoError> {
+        // Connect to remote server
+        let mut remote_socket = TcpStream::connect(remote_addr)?;
+        let remote_name = remote_addr.split_terminator(":").next().unwrap_or(remote_addr);
+
+        // Create listening socket that the enclave can connect to
+        let proxy_server = TcpListener::bind("127.0.0.1:0")?;
+        let proxy_server_port = proxy_server.local_addr()?.port();
+
+        // Notify the enclave on which port her proxy is listening on
+        let response = Response::Connected {
+                port: proxy_server_port,
+                local_addr: enclave.local_addr()?.to_string(),
+                peer_addr: enclave.peer_addr()?.to_string(),
+            };
+        Self::log_communication(
+            "runner",
+            enclave.local_addr().map(|addr| addr.port()).unwrap_or_default(),
+            "enclave",
+            enclave.peer_addr().map(|addr| addr.port()).unwrap_or_default(),
+            &format!("{:?}", &response),
+            "->");
+        enclave.write(&serde_cbor::ser::to_vec(&response).unwrap())?;
+
+        // Wait for incoming connection from enclave
+        let mut proxy = proxy_server.incoming().next().unwrap()?;
+
+        // Pass messages between remote server <-> enclave
+        loop {
+            let mut fd_set = FdSet::new();
+            fd_set.insert(proxy.as_raw_fd());
+            fd_set.insert(remote_socket.as_raw_fd());
+            select(None, Some(&mut fd_set), None, None, None).unwrap();
+
+            if fd_set.contains(proxy.as_raw_fd()) {
+                if Self::transfer_data(&mut proxy, "proxy", &mut remote_socket, remote_name).is_err() {
+                    break;
+                }
+            }
+            if fd_set.contains(remote_socket.as_raw_fd()) {
+                if Self::transfer_data(&mut remote_socket, remote_name, &mut proxy, "proxy").is_err() {
+                    break;
+                }
+            }
+        }
+        Ok(())
     }
 
     fn handle_client(stream: &mut TcpStream) -> Result<(), IoError> {
-        let request = Self::read_request(stream)?;
-        let response = match request {
-            Request::Connect{ addr } => {
-                println!("Received connect request to : {:?}", addr);
-                Response::Connected {
-                    port: 42,
-                    local_addr: "local".to_string(),
-                    peer_addr: "peer".to_string()
-                }
-            },
+        match Self::read_request(stream) {
+            Ok(Request::Connect{ addr }) => Self::handle_request_connect(&addr, stream)?,
+            Err(_e)                      => return Err(IoError::new(IoErrorKind::InvalidInput, "Failed to read request")),
         };
-        stream.write(&response.serialize().unwrap())?;
         Ok(())
     }
 
@@ -53,19 +151,14 @@ impl Server {
         let listener = TcpListener::bind(format!("127.0.0.1:{}", self.port))?;
 
         for stream in listener.incoming() {
-            let result = stream
-                .map_err(|e| format!("Failed to connect to client: {}", e))
-                .map(|mut stream| thread::Builder::new()
-                    .spawn(move || {
-                        if let Err(e) = Self::handle_client(&mut stream) {
-                            eprintln!("Error handling connection: {}, shutting connection down", e);
-                            let _ = stream.shutdown(Shutdown::Both);
-                        }
-                    }).map_err(|_| "Launch failed"));
-
-            if let Err(e) = result {
-                eprintln!("Error: {}", e);
-            }
+            let mut stream = stream?;
+            thread::Builder::new()
+                .spawn(move || {
+                    if let Err(e) = Self::handle_client(&mut stream) {
+                        eprintln!("Error handling connection: {}, shutting connection down", e);
+                        let _ = stream.shutdown(Shutdown::Both);
+                    }
+                })?;
         }
         Ok(())
     }
--- FortanixVME/fortanix-vme-abi/src/lib.rs
+++ FortanixVME/fortanix-vme-abi/src/lib.rs
@@ -1,15 +1,23 @@
 #![no_std]
-#![allow(unused)]
 
 extern crate alloc;
 
-use serde_cbor::{Error, Serializer};
-use serde_cbor::ser::SliceWrite;
+#[cfg(feature="std")]
+extern crate std;
+
+use core::fmt::{self, Display};
+use core::convert::{TryFrom, TryInto};
 use alloc::vec::Vec;
 use alloc::string::String;
 use serde::{Deserialize, Serialize};
+#[cfg(feature="std")]
+use std::io::{Error as IoError, ErrorKind, Read, Write};
+#[cfg(feature="std")]
+use std::net::TcpStream;
 
 pub const SERVER_PORT: u16 = 1024;
+#[cfg(feature="std")]
+const BUFF_SIZE: usize = 2048;
 
 #[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub enum Request {
@@ -18,13 +26,19 @@ pub enum Request {
     },
 }
 
-impl Request{
-    pub fn serialize(&self) -> Result<Vec<u8>, Error> {
-        serde_cbor::ser::to_vec(self)
-    }
+impl TryFrom<&[u8]> for Request {
+    type Error = Error;
 
-    pub fn deserialize(req: &Vec<u8>) -> Result<Self, Error> {
-        serde_cbor::from_slice(req)
+    fn try_from(request: &[u8]) -> Result<Request, Error> {
+        serde_cbor::from_slice(&request).map_err(|e| Error::DeserializationError(e))
+    }
+}
+
+impl TryInto<Vec<u8>> for Request {
+    type Error = Error;
+
+    fn try_into(self) -> Result<Vec<u8>, Error> {
+        serde_cbor::ser::to_vec(&self).map_err(|e| Error::SerializationError(e))
     }
 }
 
@@ -37,12 +51,113 @@ pub enum Response {
     },
 }
 
-impl Response {
-    pub fn serialize(&self) -> Result<Vec<u8>, Error> {
-        serde_cbor::ser::to_vec(self)
+impl TryFrom<&[u8]> for Response {
+    type Error = Error;
+
+    fn try_from(response: &[u8]) -> Result<Response, Error> {
+        serde_cbor::from_slice(&response).map_err(|e| Error::DeserializationError(e))
+    }
+}
+
+impl TryInto<Vec<u8>> for Response {
+    type Error = Error;
+
+    fn try_into(self) -> Result<Vec<u8>, Error> {
+        serde_cbor::ser::to_vec(&self).map_err(|e| Error::SerializationError(e))
+    }
+}
+
+#[derive(Debug)]
+pub enum Error {
+    SerializationError(serde_cbor::Error),
+    DeserializationError(serde_cbor::Error),
+    #[cfg(feature="std")]
+    ConnectionError(IoError),
+    AlreadyConnected,
+    NotConnected,
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "todo")
+    }
+}
+
+#[cfg(feature="std")]
+impl From<IoError> for Error {
+    fn from(e: IoError) -> Error {
+        Error::ConnectionError(e)
+    }
+}
+
+pub struct Client {
+    #[cfg(feature="std")]
+    stream: Option<TcpStream>,
+}
+
+#[cfg(feature="std")]
+impl Client {
+    pub fn new() -> Self {
+        Client {
+            stream: None,
+        }
     }
 
-    pub fn deserialize(res: &Vec<u8>) -> Result<Self, Error> {
-        serde_cbor::from_slice(res)
+    pub fn connect(&mut self) -> Result<(), Error> {
+        if let Some(_) = self.stream {
+            return Err(Error::AlreadyConnected);
+        }
+
+        let stream = TcpStream::connect(alloc::format!("localhost:{}", SERVER_PORT))?;
+        self.stream = Some(stream);
+        Ok(())
+    }
+
+    pub fn send(&mut self, req: Request) -> Result<Response, Error> {
+        if let Some(stream) = self.stream.as_mut() {
+            let req: Vec<u8> = req.try_into()?;
+            stream.write(&req)?;
+
+            let response = self.read_from_stream().unwrap();
+            Response::try_from(response.as_slice())
+        } else {
+            Err(Error::NotConnected)
+        }
+    }
+
+    fn read_from_stream(&mut self) -> Result<Vec<u8>, IoError> {
+        let mut buff = [0; BUFF_SIZE];
+        let n = self.read(&mut buff)?;
+        let mut buff = buff[0..n].to_vec();
+        //TODO This will block when the n*BUFF_SIZE bytes need to be read
+        if n == BUFF_SIZE {
+            buff.append(&mut self.read_from_stream()?);
+        }
+        Ok(buff)
+    }
+
+    fn to_inner(&mut self) -> Result<&mut TcpStream, Error> {
+        self.stream.as_mut().ok_or(Error::NotConnected)
+    }
+}
+
+#[cfg(feature="std")]
+impl Read for Client {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize, IoError> {
+        let socket = self.to_inner().map_err(|_| IoError::new(ErrorKind::NotConnected, ""))?;
+        socket.read(buf)
+    }
+}
+
+#[cfg(feature="std")]
+impl Write for Client {
+    fn write(&mut self, buf: &[u8]) -> Result<usize, IoError> {
+        let socket = self.to_inner().map_err(|_| IoError::new(ErrorKind::NotConnected, ""))?;
+        socket.write(buf)
+    }
+
+    fn flush(&mut self) -> Result<(), IoError> {
+        let socket = self.to_inner().map_err(|_| IoError::new(ErrorKind::NotConnected, ""))?;
+        socket.flush()
     }
 }
