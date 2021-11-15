19001ce3c3d34bb963fd7086508b0c0dc782672a
Raoul Strackx <raoul.strackx@fortanix.com>
2021-11-15 17:55:49 +0100

    Verbose output when runner fails

--- fortanix-vme/fortanix-vme-runner/src/main.rs
+++ fortanix-vme/fortanix-vme-runner/src/main.rs
@@ -1,7 +1,11 @@
 use fortanix_vme_runner::Server;
 use fortanix_vme_abi::SERVER_PORT;
+use std::io::ErrorKind;
 
 fn main() {
-    let (server_thread, _port) = Server::run(SERVER_PORT).expect("Server failed");
-    server_thread.join().expect("Server panicked");
+    match Server::run(SERVER_PORT) {
+        Ok((server_thread, _port))                   => server_thread.join().expect("Server panicked"),
+        Err(e) if e.kind() == ErrorKind::AddrInUse   => println!("Server failed. Have you already have a runner running on vsock port {}? Error: {:?}", SERVER_PORT, e),
+        Err(e)                                       => println!("Server failed. Error: {:?}", e),
+    }
 }
