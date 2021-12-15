ed0e8314872d25874846fa1197597db3af99f9b7
Jethro Beekman <jethro@fortanix.com>
2021-12-15 15:04:38 +0100

    Publish IAS crate

---
+++ intel-sgx/ias/src/lib.rs
@@ -0,0 +1,35 @@
+/* Copyright (c) Fortanix, Inc.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#![deny(warnings)]
+//! The normal flow for using IAS is to create an IAS client with
+//! `client::ClientBuilder::build()`, call the `get_sig_rl` and `verify_quote`
+//! APIs, then call `verifier::verify_report`.
+
+#[cfg(all(test,target_env = "sgx"))]
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate log;
+#[macro_use]
+extern crate bitflags;
+
+pub mod api;
+#[cfg(feature = "client")]
+pub mod client;
+pub mod verifier;
+
+use std::fmt;
+
+struct HexPrint<'a>(&'a [u8]);
+impl<'a> fmt::Display for HexPrint<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for b in self.0 {
+            write!(f, "{:02x}", b)?
+        }
+        Ok(())
+    }
+}
