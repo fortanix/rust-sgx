1a4b156c85236dc74626d9b00749ff21e88119ef
Jethro Beekman <jethro@fortanix.com>
2021-12-16 13:50:36 +0100

    Switch to stable asm!

--- intel-sgx/enclave-runner/src/lib.rs
+++ intel-sgx/enclave-runner/src/lib.rs
@@ -5,7 +5,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #![deny(warnings)]
-#![feature(asm)]
 #![doc(
     html_logo_url = "https://edp.fortanix.com/img/docs/edp-logo.svg",
     html_favicon_url = "https://edp.fortanix.com/favicon.ico",
