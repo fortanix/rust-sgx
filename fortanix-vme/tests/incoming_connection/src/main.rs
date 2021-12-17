389222c8200cb7ada44f45d521e5d6b9516ec35d
Raoul Strackx <raoul.strackx@fortanix.com>
2021-12-17 08:55:22 +0100

    Test stream info

--- fortanix-vme/tests/incoming_connection/src/main.rs
+++ fortanix-vme/tests/incoming_connection/src/main.rs
@@ -1,5 +1,6 @@
-use std::io::{Read, Write};
-use std::net::{IpAddr, Ipv4Addr, Shutdown, SocketAddr, TcpListener};
+#![feature(io_error_uncategorized)]
+use std::io::{ErrorKind, Read, Write};
+use std::net::{IpAddr, Ipv4Addr, Shutdown, SocketAddr, TcpListener, TcpStream};
 use std::os::unix::io::{AsRawFd, FromRawFd};
 
 fn server_run() {
@@ -15,12 +16,23 @@ fn server_run() {
     for id in 1..3 {
         println!("Waiting for connection {}", id);
         match listener.accept() {
-            Ok((mut stream, addr)) => {
+            Ok((stream, addr)) => {
                 println!("# addr = {:?}", addr);
                 assert_eq!(stream.peer_addr().unwrap().ip(), Ipv4Addr::new(127, 0, 0, 1));
                 assert!(stream.peer_addr().unwrap().port() != 3400);
                 assert_eq!(stream.local_addr().unwrap().ip(), Ipv4Addr::new(127, 0, 0, 1));
                 assert_eq!(stream.local_addr().unwrap().port(), 3400);
+
+                let fd = stream.as_raw_fd();
+                let mut stream = unsafe { TcpStream::from_raw_fd(fd) };
+                assert_eq!(stream.peer_addr().unwrap().ip(), Ipv4Addr::new(127, 0, 0, 1));
+                assert!(stream.peer_addr().unwrap().port() != 3400);
+                assert_eq!(stream.local_addr().unwrap().ip(), Ipv4Addr::new(127, 0, 0, 1));
+                assert_eq!(stream.local_addr().unwrap().port(), 3400);
+
+                let no_stream = unsafe { TcpStream::from_raw_fd(666.into()) };
+                assert_eq!(no_stream.peer_addr().unwrap_err().kind(), ErrorKind::Uncategorized);
+
                 println!("Connection {}: Connected", id);
                 let mut buff_in = [0u8; 4192];
                 let n = stream.read(&mut buff_in).unwrap();
