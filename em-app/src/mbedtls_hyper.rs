2e1218db79396d285aae01ff07f3a9fbe6e1b165
Yuxiang Cao <yuxiang.cao@fortanix.com>
2023-04-25 13:41:08 -0700

    update code for api changes in `mbedtls`

--- em-app/src/mbedtls_hyper.rs
+++ em-app/src/mbedtls_hyper.rs
@@ -6,42 +6,47 @@
 use hyper::net::{NetworkStream, SslClient, SslServer};
 use std::fmt;
 use std::io;
-use std::marker::PhantomData;
+use std::io::{Error as IoError, ErrorKind as IoErrorKind};
 use std::net::SocketAddr;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
-use std::io::{Error as IoError, ErrorKind as IoErrorKind};
 
 use mbedtls::ssl::{Config, Context};
 
 // Native TLS compatibility - to move to native tls client in the future
 #[derive(Clone)]
 pub struct TlsStream<T> {
-    context: Arc<Mutex<Context>>,
-    phantom: PhantomData<T>,
+    context: Arc<Mutex<Context<T>>>,
 }
 
-impl<T: 'static> TlsStream<T> {
-    pub fn new(context: Arc<Mutex<Context>>) -> io::Result<Self> {
-        if context.lock().unwrap().io_mut().map(|v| v.downcast_ref::<T>()).is_none() {
-            return Err(IoError::new(IoErrorKind::InvalidInput, "Peer set in context is not of expected type"));
+impl<T> TlsStream<T>
+where
+    T: 'static,
+{
+    pub fn new(context: Arc<Mutex<Context<T>>>) -> io::Result<Self> {
+        if context.lock().unwrap().io_mut().is_none() {
+            return Err(IoError::new(
+                IoErrorKind::InvalidInput,
+                "Peer set in context is not of expected type",
+            ));
         }
 
-        Ok(TlsStream {
-            context: context,
-            phantom: PhantomData,
-        })
+        Ok(TlsStream { context: context })
     }
 }
 
-impl<T: 'static> io::Read for TlsStream<T>
+impl<T> io::Read for TlsStream<T>
+where
+    T: 'static + io::Read + io::Write,
 {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         self.context.lock().unwrap().read(buf)
     }
 }
 
-impl<T: 'static> io::Write for TlsStream<T>
+impl<T> io::Write for TlsStream<T>
+where
+    T: 'static + io::Read + io::Write,
 {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
         self.context.lock().unwrap().write(buf)
@@ -52,31 +57,37 @@ impl<T: 'static> io::Write for TlsStream<T>
     }
 }
 
-impl<T: 'static> NetworkStream for TlsStream<T>
-    where T: NetworkStream
+impl<T> NetworkStream for TlsStream<T>
+where
+    T: NetworkStream + 'static,
 {
     fn peer_addr(&mut self) -> io::Result<SocketAddr> {
-        self.context.lock().unwrap().io_mut()
+        self.context
+            .lock()
+            .unwrap()
+            .io_mut()
             .ok_or(IoError::new(IoErrorKind::NotFound, "No peer available"))?
-            .downcast_mut::<T>().expect("Software error, unexpected type stored in io_mut")
             .peer_addr()
     }
-    
+
     fn set_read_timeout(&self, dur: Option<Duration>) -> io::Result<()> {
-        self.context.lock().unwrap().io_mut()
+        self.context
+            .lock()
+            .unwrap()
+            .io_mut()
             .ok_or(IoError::new(IoErrorKind::NotFound, "No peer available"))?
-            .downcast_mut::<T>().expect("Software error, unexpected type stored in io_mut")
             .set_read_timeout(dur)
     }
 
     fn set_write_timeout(&self, dur: Option<Duration>) -> io::Result<()> {
-        self.context.lock().unwrap().io_mut()
+        self.context
+            .lock()
+            .unwrap()
+            .io_mut()
             .ok_or(IoError::new(IoErrorKind::NotFound, "No peer available"))?
-            .downcast_mut::<T>().expect("Software error, unexpected type stored in io_mut")
             .set_write_timeout(dur)
     }
- }
-
+}
 
 #[derive(Clone)]
 pub struct MbedSSLServer {
@@ -85,19 +96,19 @@ pub struct MbedSSLServer {
 
 impl MbedSSLServer {
     pub fn new(rc_config: Arc<Config>) -> Self {
-        MbedSSLServer {
-            rc_config,
-        }
+        MbedSSLServer { rc_config }
     }
 }
 
-/// An abstraction to allow any SSL implementation to be used with server-side HttpsStreams.
+/// An abstraction to allow any SSL implementation to be used with server-side
+/// HttpsStreams.
 impl<T> SslServer<T> for MbedSSLServer
-    where T: NetworkStream + Send + Clone + fmt::Debug + Sync
+where
+    T: NetworkStream + Send + Clone + fmt::Debug + Sync,
 {
     /// The protected stream.
     type Stream = TlsStream<T>;
-    
+
     /// Wrap a server stream with SSL.
     fn wrap_server(&self, stream: T) -> Result<Self::Stream, hyper::Error> {
         let mut ctx = Context::new(self.rc_config.clone());
@@ -113,7 +124,8 @@ pub struct MbedSSLClient {
     verify_hostname: bool,
 
     // This can be used when verify_hostname is set to true.
-    // It will force ssl client to send this specific SNI on all established connections disregarding any host provided by hyper.
+    // It will force ssl client to send this specific SNI on all established connections disregarding any host provided by
+    // hyper.
     override_sni: Option<String>,
 }
 
@@ -126,7 +138,7 @@ impl MbedSSLClient {
             override_sni: None,
         }
     }
-    
+
     #[allow(dead_code)]
     pub fn new_with_sni(rc_config: Arc<Config>, verify_hostname: bool, override_sni: Option<String>) -> Self {
         MbedSSLClient {
@@ -138,7 +150,8 @@ impl MbedSSLClient {
 }
 
 impl<T> SslClient<T> for MbedSSLClient
-    where T: NetworkStream + Send + Clone + fmt::Debug + Sync
+where
+    T: NetworkStream + Send + Clone + fmt::Debug + Sync,
 {
     type Stream = TlsStream<T>;
 
@@ -149,11 +162,10 @@ impl<T> SslClient<T> for MbedSSLClient
             true => Some(self.override_sni.as_ref().map(|v| v.as_str()).unwrap_or(host)),
             false => None,
         };
-        
+
         match context.establish(stream, verify_hostname) {
             Ok(()) => Ok(TlsStream::new(Arc::new(Mutex::new(context))).expect("Software error creating TlsStream")),
             Err(e) => Err(hyper::Error::Ssl(Box::new(e))),
         }
     }
 }
-
