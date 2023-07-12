cf147f6bf1e25f365d9de61e7db9af89301fa258
John Foley <john.foley@fortanix.com>
2023-07-12 17:54:49 -0400

    Support for attaching identity to Nitro Debug Enclaves.
     - Define new OID for Nitro PCR0
     - Allow specification of DEBUG_PCR0 environment variable (only in debug enclaves).
     - Wrap nsm_driver in a cleaner API.
     - Also make em-app::error::Error impl std::error::Error

--- fortanix-vme/vme-pkix/src/oid.rs
+++ fortanix-vme/vme-pkix/src/oid.rs
@@ -4,10 +4,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#![allow(non_upper_case_globals)]
-
 use pkix::types::ObjectIdentifier;
 
-lazy_static!{
+lazy_static! {
+    /// This OID identifies an extension containing a Nitro attestation report.
+    /// Note that all OIDs beginning with 1.3.6.1.4.1.49690 are registered to Fortanix.
     pub static ref ATTESTATION_NITRO: ObjectIdentifier = vec![1, 3, 6, 1, 4, 1, 49690, 2, 2, 6].into();
+    /// This OID identifies an extension containing the PCR0 of a Nitro enclave;
+    /// used to insecurely provide identification to debug attestations that
+    /// otherwise contain all zeroes in their PCR0 measurement.
+    pub static ref ATTESTATION_NITRO_PCR0: ObjectIdentifier = vec![1, 3, 6, 1, 4, 1, 49690, 2, 2, 6, 1].into();
 }
