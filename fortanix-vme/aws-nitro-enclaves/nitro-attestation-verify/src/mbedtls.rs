c130f9f09e6afa59e20792aa3879323e46efb9d3
Yuxiang Cao <yuxiang.cao@fortanix.com>
2023-12-21 10:19:48 -0800

    upgrade mbedtls to 0.12.1

--- fortanix-vme/aws-nitro-enclaves/nitro-attestation-verify/src/mbedtls.rs
+++ fortanix-vme/aws-nitro-enclaves/nitro-attestation-verify/src/mbedtls.rs
@@ -1,7 +1,7 @@
-/* Copyright (c) Fortanix, Inc.                                                                                                                                                                                                          
- *                                                                                                                                                                                                                                       
- * This Source Code Form is subject to the terms of the Mozilla Public                                                                                                                                                                   
- * License, v. 2.0. If a copy of the MPL was not distributed with this                                                                                                                                                                   
+/* Copyright (c) Fortanix, Inc.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 use aws_nitro_enclaves_cose::crypto::{Hash, MessageDigest, SignatureAlgorithm, SigningPublicKey};
 use aws_nitro_enclaves_cose::error::CoseError;
