bbd6424177f8cd12f90cfd18703f6e6e6084eb3a
Yuxiang Cao <yuxiang.cao@fortanix.com>
2023-12-22 13:31:22 -0800

    update & fix example tls

--- examples/tls/src/main.rs
+++ examples/tls/src/main.rs
@@ -5,6 +5,8 @@ extern crate mbedtls;
 
 use chrono::prelude::*;
 
+use mbedtls::alloc::Box as MbedtlsBox;
+use mbedtls::alloc::List as MbedtlsList;
 use mbedtls::hash::Type::Sha256;
 use mbedtls::pk::Pk;
 use mbedtls::rng::Rdrand;
@@ -13,15 +15,15 @@ use mbedtls::ssl::{Config, Context};
 use mbedtls::x509::certificate::{Builder, Certificate};
 use mbedtls::x509::Time;
 use mbedtls::Result as TlsResult;
-use std::io::Write;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
+use std::sync::Arc;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 const RSA_KEY_SIZE: u32 = 3072;
 const RSA_KEY_EXP: u32 = 0x10001;
 const DAYS_TO_SES: u64 = 86400;
-const CERT_VAL_SECS: u64 = (365 * DAYS_TO_SES);
+const CERT_VAL_SECS: u64 = 365 * DAYS_TO_SES;
 
 trait ToTime {
     fn to_time(&self) -> Time;
@@ -47,8 +49,8 @@ fn get_validity() -> (Time, Time) {
         .unwrap()
         .as_secs();
     let end = start + CERT_VAL_SECS;
-    let not_before = Utc.timestamp(start as _, 0);
-    let not_after = Utc.timestamp(end as _, 0);
+    let not_before = Utc.timestamp_opt(start as _, 0).unwrap();
+    let not_after = Utc.timestamp_opt(end as _, 0).unwrap();
     (not_before.to_time(), not_after.to_time())
 }
 
@@ -59,7 +61,7 @@ fn get_validity() -> (Time, Time) {
 /// also verify attestation information.
 /// along with traditional certificate verification.
 /// But this example doesn't show that.
-fn get_key_and_cert() -> (Pk, Certificate) {
+fn get_key_and_cert() -> (Pk, MbedtlsBox<Certificate>) {
     let mut rng = Rdrand;
     let mut key = Pk::generate_rsa(&mut rng, RSA_KEY_SIZE, RSA_KEY_EXP).unwrap();
     let mut key_i = Pk::generate_rsa(&mut rng, RSA_KEY_SIZE, RSA_KEY_EXP).unwrap();
@@ -89,18 +91,13 @@ fn get_key_and_cert() -> (Pk, Certificate) {
 /// a self signed certificate.
 /// After a session is established, echo the incoming stream to the client.
 /// till EOF is detected.
-fn serve(mut conn: TcpStream, key: &mut Pk, cert: &mut Certificate) -> TlsResult<()> {
-    let mut rng = Rdrand;
-
-    let mut config = Config::new(Endpoint::Server, Transport::Stream, Preset::Default);
-    config.set_rng(Some(&mut rng));
-    config.push_cert(&mut **cert, key)?;
-    let mut ctx = Context::new(&config)?;
+fn serve(conn: TcpStream, config: Arc<Config>) -> TlsResult<()> {
+    let mut ctx = Context::new(config);
 
     let mut buf = String::new();
-    let session = ctx.establish(&mut conn, None)?;
+    ctx.establish(conn, None)?;
     println!("Connection established!");
-    let mut reader = BufReader::new(session);
+    let mut reader = BufReader::new(ctx);
     while let Ok(1..=std::usize::MAX) = reader.read_line(&mut buf) {
         let session = reader.get_mut();
         session.write_all(&buf.as_bytes()).unwrap();
@@ -109,14 +106,29 @@ fn serve(mut conn: TcpStream, key: &mut Pk, cert: &mut Certificate) -> TlsResult
     Ok(())
 }
 
+fn create_server_config(
+    cert: MbedtlsBox<Certificate>,
+    key: Pk,
+) -> Result<Arc<Config>, mbedtls::Error> {
+    let rng = Rdrand;
+    let mut config = Config::new(Endpoint::Server, Transport::Stream, Preset::Default);
+    config.set_rng(Arc::new(rng));
+    let mut certs = MbedtlsList::new();
+    certs.push(cert);
+    config.push_cert(Arc::new(certs), Arc::new(key))?;
+    let config = Arc::new(config);
+    Ok(config)
+}
+
 /// The below main() starts a TLS echo server on `local host:7878`.
 fn main() {
-    let (mut key, mut cert) = get_key_and_cert();
+    let (key, cert) = get_key_and_cert();
+    let config = create_server_config(cert, key).unwrap();
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        let _ = serve(stream, &mut key, &mut cert).unwrap();
+        let _ = serve(stream, config.clone()).unwrap();
         println!("Connection closed!");
     }
 }
