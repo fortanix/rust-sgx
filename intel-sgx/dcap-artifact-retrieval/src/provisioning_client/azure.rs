ea3b018e4cf13025e0377ef48b055d61548cd741
NikitaShyrei <nikita.shyrei@fortanix.com>
2024-07-29 15:18:51 +0200

    - added licence headers

--- intel-sgx/dcap-artifact-retrieval/src/provisioning_client/azure.rs
+++ intel-sgx/dcap-artifact-retrieval/src/provisioning_client/azure.rs
@@ -1,3 +1,10 @@
+/* Copyright (c) Fortanix, Inc.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 use pcs::{CpuSvn, EncPpid, PceId, PceIsvsvn, PckCert, PckCerts, QeId, Unverified};
 use rustc_serialize::hex::ToHex;
 use serde::Deserialize;
--- intel-sgx/dcap-artifact-retrieval/src/provisioning_client/intel.rs
+++ intel-sgx/dcap-artifact-retrieval/src/provisioning_client/intel.rs
@@ -1,3 +1,10 @@
+/* Copyright (c) Fortanix, Inc.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 //! Interface to the Intel DCAP attestation API
 //! Origins:
 //!   - https://api.portal.trustedservices.intel.com/provisioning-certification
