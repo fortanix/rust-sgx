bb1058a37bafe64c9a1311f6f266036b31fe406e
Raoul Strackx <raoul.strackx@fortanix.com>
2024-08-14 16:10:21 +0200

    Allow unexpected cfgs

--- em-app/src/lib.rs
+++ em-app/src/lib.rs
@@ -3,6 +3,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+#![allow(unexpected_cfgs)] // fortanixvme platform hasn't been upstreamed yet, but used in cfg
 pub extern crate serde_derive;
 
 pub mod mbedtls_hyper;
