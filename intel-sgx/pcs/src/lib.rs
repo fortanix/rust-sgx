98f86515527971afcf556f9784a67f756ecfcb93
Raoul Strackx <raoul.strackx@fortanix.com>
2025-04-29 17:00:01 +0200

    Expose TcbLevel type

--- intel-sgx/pcs/src/lib.rs
+++ intel-sgx/pcs/src/lib.rs
@@ -30,7 +30,7 @@ use {
 pub use crate::pckcrl::PckCrl;
 pub use crate::pckcrt::{PckCert, PckCerts, SGXPCKCertificateExtension, SGXType};
 pub use crate::qe_identity::{EnclaveIdentity, QeIdentity, QeIdentitySigned};
-pub use crate::tcb_info::{AdvisoryID, Fmspc, TcbInfo, TcbData};
+pub use crate::tcb_info::{AdvisoryID, Fmspc, TcbInfo, TcbData, TcbLevel};
 pub use crate::tcb_evaluation_data_numbers::{RawTcbEvaluationDataNumbers, TcbEvalNumber, TcbEvaluationDataNumbers, TcbPolicy};
 
 mod io;
