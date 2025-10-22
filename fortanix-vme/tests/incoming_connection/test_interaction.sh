bd85f368182e45d29daf8c1f2260b2d9cb8ebbe2
aditijannu <aditi.jannu@fortanix.com>
2025-10-22 09:33:16 +0200

    Remove fortanix-vme/tests. The target is tested against the compiler testsuite

--- fortanix-vme/tests/incoming_connection/test_interaction.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -ex
-
-while [ true ]
-do
-  echo "Interacting with test"
-  timeout 1s curl -k localhost:3400 || true
-  sleep 5s
-done
--- fortanix-vme/tests/iron/test_interaction.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -ex
-
-while [ true ]
-do
-  echo "Interacting with test"
-  timeout 1s curl -k localhost:3000 || true
-  sleep 5s
-done
