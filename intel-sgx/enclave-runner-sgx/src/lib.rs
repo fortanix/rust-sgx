4de5df23a017a69b8b98a3f50ff5863328827b2b
Gilang Mentari Hamidy <gilang.hamidy@fortanix.com>
2026-02-06 13:57:25 +0100

    Add feature flag to allow older compiler compiling renamed IOError correctly

--- intel-sgx/enclave-runner-sgx/src/lib.rs
+++ intel-sgx/enclave-runner-sgx/src/lib.rs
@@ -3,7 +3,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
+#![cfg_attr(feature = "err-compat", feature(io_error_more))]
 #![allow(non_local_definitions)] // Required by failure
 #![deny(warnings)]
 #![doc(
