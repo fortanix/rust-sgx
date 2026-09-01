ab070cea7dfae7cac5ded0c0da7311f11d90b400
Sinan Nalkaya <sinan.nalkaya@fortanix.com>
2026-09-01 19:46:23 +0200

    RTE-1179: Use rustc_version for rustc version.

--- intel-sgx/async-usercalls/build.rs
+++ intel-sgx/async-usercalls/build.rs
@@ -1,28 +1,12 @@
-use std::env;
-use std::process::Command;
-use std::str;
+use rustc_version::version;
 
 // Enables configuration for non-stable features that are stabilized later.
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rustc-check-cfg=cfg(no_never_type)");
+    let rustc_version = version().unwrap();
 
-    let minor = match rustc_minor_version() {
-        Some(minor) => minor,
-        None => return,
-    };
-    if minor < 100 {
+    if rustc_version.minor < 100 {
         println!("cargo:rustc-cfg=no_never_type");
     }
-}
-
-fn rustc_minor_version() -> Option<u32> {
-    let rustc = env::var_os("RUSTC")?;
-    let output = Command::new(rustc).arg("--version").output().ok()?;
-    let version = str::from_utf8(&output.stdout).ok()?;
-    let mut pieces = version.split('.');
-    if pieces.next() != Some("rustc 1") {
-        return None;
-    }
-    pieces.next()?.parse().ok()
 }
